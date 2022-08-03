a67d33015f2795855dde6cf9b69207a8e02d0a82
nipat01 <nipat.pueksasuwan@gmail.com>
2022-08-04 00:04:30 +0700

    add env

--- simpleWebApp.go
+++ simpleWebApp.go
@@ -1,17 +1,46 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
+	"log"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
 	r := gin.Default()
+	r.Use(CORSMiddleware())
+
+	var envs map[string]string
+	envs, err := godotenv.Read(".env")
+	if err != nil {
+		log.Println("Error to load .env file")
+	}
+	log.Println("envs: ", envs)
+
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"data": "hello world go gin kubernetes",
 		})
 	})
 
-	r.Run()
+	r.Run(":" + envs["PORT"])
+}
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Credentials", "true")
+		c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Header("Access-Control-Allow-Methods", "POST,HEAD,PATCH, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
 }
